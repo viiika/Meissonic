808d05f30007237e7d23ce6bc159762a2f766691
Owen718 <57895293+Owen718@users.noreply.github.com>
2024-10-14 22:32:46 +0800

    Update inference.py

--- inference.py
+++ inference.py
@@ -26,34 +26,24 @@ pipe=Pipeline(vq_model, tokenizer=tokenizer,text_encoder=text_encoder,transforme
 
 pipe = pipe.to(device)
 
-steps = 48
+steps = 64
 CFG = 9
 resolution = 1024 
-negative_prompts = "worst quality, normal quality, low quality, low res, blurry, distortion, text, watermark, logo, banner, extra digits, cropped, jpeg artifacts, signature, username, error, sketch, duplicate, ugly, monochrome, horror, geometry, mutation, disgusting, bad anatomy, bad proportions, bad quality, deformed, disconnected limbs, out of frame, out of focus, dehydrated, disfigured, extra arms, extra limbs, extra hands, fused fingers, gross proportions, long neck, jpeg, malformed limbs, mutated, mutated hands, mutated limbs, missing arms, missing fingers, picture frame, poorly drawn hands, poorly drawn face, collage, pixel, pixelated, grainy, color aberration, amputee, autograph, bad illustration, beyond the borders, blank background, body out of frame, boring background, branding, cut off, dismembered, disproportioned, distorted, draft, duplicated features, extra fingers, extra legs, fault, flaw, grains, hazy, identifying mark, improper scale, incorrect physiology, incorrect ratio, indistinct, kitsch, low resolution"
+negative_prompts = "worst quality, low quality, low res, blurry, distortion, watermark, logo, signature, text, jpeg artifacts, signature, sketch, duplicate, ugly, identifying mark"
 
+prompts = [
+    "Two actors are posing for a pictur with one wearing a black and white face paint.",
+    "A large body of water with a rock in the middle and mountains in the background.",
+    "A white and blue coffee mug with a picture of a man on it.",
+    "A statue of a man with a crown on his head.",
+    "A man in a yellow wet suit is holding a big black dog in the water.",
+    "A white table with a vase of flowers and a cup of coffee on top of it.",
+    "A woman stands on a dock in the fog.",
+    "A woman is standing next to a picture of another woman."
+]
 
-# A racoon wearing a suit smoking a cigar in the style of James Gurney.
-# Medieval painting of a rat king.
-# Oil portrait of Super Mario as a shaman tripping on mushrooms in a dark and detailed scene.
-# A painting of a Persian cat dressed as a Renaissance king, standing on a skyscraper overlooking a city.
-# A fluffy owl sits atop a stack of antique books in a detailed and moody illustration.
-# A cosmonaut otter poses for a portrait painted in intricate detail by Rembrandt.
-# A painting featuring a woman wearing virtual reality glasses and a bird, created by Dave McKean and Ivan Shishkin.
-# A hyperrealist portrait of a fairy girl emperor wearing a crown and long starry robes.
-# A psychedelic painting of a fantasy space whale.
-# A monkey in a blue top hat painted in oil by Vincent van Gogh in the 1800s.
-# A queen with red hair and a green and black dress stands veiled in a highly detailed and elegant digital painting.
-# An oil painting of an anthropomorphic fox overlooking a village in the moor.
-# A digital painting of an evil geisha in a bar.
-# Digital painting of a furry deer character on FurAffinity.
-# A highly detailed goddess portrait with a focus on the eyes.
-# A cute young demon princess in a forest, depicted in digital painting.
-# A red-haired queen wearing a green and black dress and veil is depicted in an intricate and elegant digital painting.
-prompt = "A racoon wearing a suit smoking a cigar in the style of James Gurney."
-
-image = pipe(prompt=prompt,negative_prompt=negative_prompts,height=resolution,width=resolution,guidance_scale=CFG,num_inference_steps=steps).images[0]
+image = pipe(prompt=prompts[0],negative_prompt=negative_prompts,height=resolution,width=resolution,guidance_scale=CFG,num_inference_steps=steps).images[0]
 
 output_dir = "./output"
 os.makedirs(output_dir, exist_ok=True)
 image.save(output_dir, f"{prompt[:10]}_{resolution}_{steps}_{CFG}.png")
-
