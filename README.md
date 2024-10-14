6679ef9a31ea39ecbfc56e0f796176ab1596eca3
JB-Bai <1355864570@qq.com>
2024-10-14 15:47:10 +0800

    fix

--- README.md
+++ README.md
@@ -1,16 +1,27 @@
 # Meissonic: Revitalizing Masked Generative Transformers for Efficient High-Resolution Text-to-Image Synthesis
 
-[Paper](https://arxiv.org/abs/) | [Code](https://github.com/viiika/Meissonic)
+[Paper](https://arxiv.org/abs/2410.08261) | [Model](https://huggingface.co/MeissonFlow/Meissonic) | [Code](https://github.com/viiika/Meissonic)
 
 
-[demo](./assets/demos.pdf)
+![demo](./assets/demos.png)
 
 ## Introduction
 Meissonic is a non-autoregressive mask image modeling text-to-image synthesis model that can generate high-resolution images. It is designed to run on consumer graphics cards.
 
+## Prerequisites
+
+```bash
+git clone https://github.com/huggingface/diffusers.git
+cd diffusers
+pip install -e .
+```
+
 ## Usage
 
-Under Construction. Please check back later.
+```bash
+python inference.py
+```
+
 
 ## Citation
 If you find this work helpful, please consider citing:
@@ -18,7 +29,7 @@ If you find this work helpful, please consider citing:
 @article{bai2024meissonic,
   title={Meissonic: Revitalizing Masked Generative Transformers for Efficient High-Resolution Text-to-Image Synthesis},
   author={Bai, Jinbin and Ye, Tian and Chow, Wei and Song, Enxin and Chen, Qing-Guo and Li, Xiangtai and Dong, Zhen and Zhu, Lei and Yan, Shuicheng},
-  journal={arXiv preprint arXiv},
+  journal={arXiv preprint arXiv:2410.08261},
   year={2024}
 }
 ```
--- inference.py
+++ inference.py
@@ -31,8 +31,25 @@ CFG = 9
 resolution = 1024 
 negative_prompts = "worst quality, normal quality, low quality, low res, blurry, distortion, text, watermark, logo, banner, extra digits, cropped, jpeg artifacts, signature, username, error, sketch, duplicate, ugly, monochrome, horror, geometry, mutation, disgusting, bad anatomy, bad proportions, bad quality, deformed, disconnected limbs, out of frame, out of focus, dehydrated, disfigured, extra arms, extra limbs, extra hands, fused fingers, gross proportions, long neck, jpeg, malformed limbs, mutated, mutated hands, mutated limbs, missing arms, missing fingers, picture frame, poorly drawn hands, poorly drawn face, collage, pixel, pixelated, grainy, color aberration, amputee, autograph, bad illustration, beyond the borders, blank background, body out of frame, boring background, branding, cut off, dismembered, disproportioned, distorted, draft, duplicated features, extra fingers, extra legs, fault, flaw, grains, hazy, identifying mark, improper scale, incorrect physiology, incorrect ratio, indistinct, kitsch, low resolution"
 
-prompt = "a beautiful landscape painting with a river and mountains in the background"
 
+# A racoon wearing a suit smoking a cigar in the style of James Gurney.
+# Medieval painting of a rat king.
+# Oil portrait of Super Mario as a shaman tripping on mushrooms in a dark and detailed scene.
+# A painting of a Persian cat dressed as a Renaissance king, standing on a skyscraper overlooking a city.
+# A fluffy owl sits atop a stack of antique books in a detailed and moody illustration.
+# A cosmonaut otter poses for a portrait painted in intricate detail by Rembrandt.
+# A painting featuring a woman wearing virtual reality glasses and a bird, created by Dave McKean and Ivan Shishkin.
+# A hyperrealist portrait of a fairy girl emperor wearing a crown and long starry robes.
+# A psychedelic painting of a fantasy space whale.
+# A monkey in a blue top hat painted in oil by Vincent van Gogh in the 1800s.
+# A queen with red hair and a green and black dress stands veiled in a highly detailed and elegant digital painting.
+# An oil painting of an anthropomorphic fox overlooking a village in the moor.
+# A digital painting of an evil geisha in a bar.
+# Digital painting of a furry deer character on FurAffinity.
+# A highly detailed goddess portrait with a focus on the eyes.
+# A cute young demon princess in a forest, depicted in digital painting.
+# A red-haired queen wearing a green and black dress and veil is depicted in an intricate and elegant digital painting.
+prompt = "A racoon wearing a suit smoking a cigar in the style of James Gurney."
 
 image = pipe(prompt=prompt,negative_prompt=negative_prompts,height=resolution,width=resolution,guidance_scale=CFG,num_inference_steps=steps).images[0]
 
