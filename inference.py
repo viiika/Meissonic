4ef55ebd104776cfaa669b9053610d9b2b785d45
wwx <42485228+ValMystletainn@users.noreply.github.com>
2024-10-15 12:10:51 +0800

    fix: the typo in inference.py that make the saving error
    the image save command is mixing some typo.
    Change:
    1. prompt[0] -> prompts[0] to visiti the correct variable
    2. image.save(output_dir, ...) -> image.save(os.path.join(outputdir, ...)),  to do the correct save

--- inference.py
+++ inference.py
@@ -49,4 +49,4 @@ image = pipe(prompt=prompts[0],negative_prompt=negative_prompts,height=resolutio
 
 output_dir = "./output"
 os.makedirs(output_dir, exist_ok=True)
-image.save(output_dir, f"{prompt[0][:10]}_{resolution}_{steps}_{CFG}.png")
+image.save(os.path.join(output_dir, f"{prompts[0][:10]}_{resolution}_{steps}_{CFG}.png"))
